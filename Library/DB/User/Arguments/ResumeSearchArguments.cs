d117310b310716a073fe97d9b7533b54b8222eba
ljymill <ljymill@d8e7726e-33f6-2583-9b26-3a411985976f>
2011-03-06 15:33:57 +0000

    유저 검색 triger 추가 검색 sp/entity 작업

---
+++ Library/DB/User/Arguments/ResumeSearchArguments.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Com.Framework.Data;
+
+namespace Com.Library.DB.User
+{
+    public class ResumeSearchArguments : DBArguments
+    {
+        public int CountryNo
+        {
+            get { return Convert.ToInt32(GetValue("CountryNo", 0)); }
+            set { SetValue("CountryNo", value); }
+        }
+
+        public string UserName
+        {
+            get { return Convert.ToString(GetValue("UserName", string.Empty)); }
+            set { SetValue("UserName", value); }
+        }
+
+        public byte JoinType
+        {
+            get { return Convert.ToByte(GetValue("JoinType", (byte)0)); }
+            set { SetValue("JoinType", value); }
+        }
+
+        public int Category1No
+        {
+            get { return Convert.ToInt32(GetValue("Category1No", 0)); }
+            set { SetValue("Category1No", value); }
+        }
+
+        public int Category2No
+        {
+            get { return Convert.ToInt32(GetValue("Category2No", 0)); }
+            set { SetValue("Category2No", value); }
+        }
+
+        public int CityCategory
+        {
+            get { return Convert.ToInt32(GetValue("CityCategory", 0)); }
+            set { SetValue("CityCategory", value); }
+        }
+
+        public int AreaCategory
+        {
+            get { return Convert.ToInt32(GetValue("AreaCategory", 0)); }
+            set { SetValue("AreaCategory", value); }
+        }
+
+        public int Age_Start
+        {
+            get { return Convert.ToInt32(GetValue("Age_Start", 0)); }
+            set { SetValue("Age_Start", value); }
+        }
+
+        public int Age_End
+        {
+            get { return Convert.ToInt32(GetValue("Age_End", 0)); }
+            set { SetValue("Age_End", value); }
+        }
+
+        public int Gender
+        {
+            get { return Convert.ToInt32(GetValue("Gender", 0)); }
+            set { SetValue("Gender", value); }
+        }
+
+        public int PageNo
+        {
+            get { return Convert.ToInt32(GetValue("PageNo", 0)); }
+            set { SetValue("PageNo", value); }
+        }
+
+        public byte PageSize
+        {
+            get { return Convert.ToByte(GetValue("PageSize", (byte)0)); }
+            set { SetValue("PageSize", value); }
+        }
+    }
+}
---
+++ Library/DB/User/Entity/ResumeSearchEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Com.Framework.Data;
+
+namespace Com.Library.DB.User
+{
+    public class ResumeSearchEntity : DataEntity
+    {
+        public int UserNo { get { return Convert.ToInt32(GetData("UserNo", (int)0)); } }
+        public int CountryNo { get { return Convert.ToInt32(GetData("CountryNo", (int)0)); } }
+        public string KRName { get { return Convert.ToString(GetData("KRName", string.Empty)); } }
+        public string CNName { get { return Convert.ToString(GetData("CNName", string.Empty)); } }
+        public string ENGName { get { return Convert.ToString(GetData("ENGName", string.Empty)); } }
+        public string UserImage { get { return Convert.ToString(GetData("UserImage", string.Empty)); } }
+        public byte JoinType { get { return Convert.ToByte(GetData("JoinType", (byte)0)); } }
+        public int Category1No { get { return Convert.ToInt32(GetData("Category1No", (int)0)); } }
+        public int Category2No { get { return Convert.ToInt32(GetData("Category2No", (int)0)); } }
+        public int CityCategory { get { return Convert.ToInt32(GetData("CityCategory", (int)0)); } }
+        public int AreaCategory { get { return Convert.ToInt32(GetData("AreaCategory", (int)0)); } }
+        public byte KoreanAge { get { return Convert.ToByte(GetData("KoreanAge", (byte)0)); } }
+        public byte Age { get { return Convert.ToByte(GetData("Age", (byte)0)); } }
+        public byte Gender { get { return Convert.ToByte(GetData("Gender", (byte)0)); } }
+        public byte Career { get { return Convert.ToByte(GetData("Career", (byte)0)); } }
+        public DateTime RegistryTime { get { return Convert.ToDateTime(GetData("RegistryTime", DateTime.MinValue)); } }
+    }
+}
