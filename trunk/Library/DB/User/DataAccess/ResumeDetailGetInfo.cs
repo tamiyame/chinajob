46b2ac73efd95eb84cbfb68629de03ee368a7f35
kizrael@gmail.com <kizrael@gmail.com@d8e7726e-33f6-2583-9b26-3a411985976f>
2011-03-07 13:45:53 +0000

    작업중

--- trunk/Library/DB/User/DataAccess/ResumeDetailGetInfo.cs
+++ trunk/Library/DB/User/DataAccess/ResumeDetailGetInfo.cs
@@ -18,16 +18,16 @@ namespace Com.Library.DB.User
 		{
 			AddParameter("UserNo", SqlDbType.Int, 4, SqlDataInOut.Input);
 			AddParameter("CountryNo", SqlDbType.Int, 4, SqlDataInOut.Input);
-			AddParameter("SSN1", SqlDbType.VarChar, 6, SqlDataInOut.Input);
-			AddParameter("KoreanAge", SqlDbType.TinyInt, 1, SqlDataInOut.Input);
-			AddParameter("Age", SqlDbType.TinyInt, 1, SqlDataInOut.Input);
-			AddParameter("ChinaExp", SqlDbType.NVarChar, 40, SqlDataInOut.Input);
-			AddParameter("Military", SqlDbType.TinyInt, 1, SqlDataInOut.Input);
-			AddParameter("IsCareer", SqlDbType.TinyInt, 1, SqlDataInOut.Input);
-			AddParameter("Address", SqlDbType.NVarChar, 400, SqlDataInOut.Input);
-			AddParameter("Description", SqlDbType.NVarChar, -1, SqlDataInOut.Input);
-			AddParameter("AboutMe", SqlDbType.NVarChar, -1, SqlDataInOut.Input);
 			AddParameter("ResumeDetailNo", SqlDbType.Int, 4, SqlDataInOut.Output);
+			AddParameter("SSN1", SqlDbType.VarChar, 6, SqlDataInOut.Output);
+			AddParameter("KoreanAge", SqlDbType.TinyInt, 1, SqlDataInOut.Output);
+			AddParameter("Age", SqlDbType.TinyInt, 1, SqlDataInOut.Output);
+			AddParameter("ChinaExp", SqlDbType.NVarChar, 40, SqlDataInOut.Output);
+			AddParameter("Military", SqlDbType.TinyInt, 1, SqlDataInOut.Output);
+			AddParameter("IsCareer", SqlDbType.TinyInt, 1, SqlDataInOut.Output);
+			AddParameter("Address", SqlDbType.NVarChar, 400, SqlDataInOut.Output);
+			AddParameter("Description", SqlDbType.NVarChar, -1, SqlDataInOut.Output);
+			AddParameter("AboutMe", SqlDbType.NVarChar, -1, SqlDataInOut.Output);
 		}
 	}
 }
--- trunk/Library/DB/User/Entity/ResumeAcademicAbilityEntity.cs
+++ trunk/Library/DB/User/Entity/ResumeAcademicAbilityEntity.cs
@@ -5,7 +5,7 @@ namespace Com.Library.DB.User
 {
 	public class ResumeAcademicAbilityEntity : DataEntity
 	{
-		public int UserNo { get { return Convert.ToInt32(GetData("UserNo", (int)0)); } }
+		public int UserNo { get { return Convert.ToInt32(GetData("UserNo", (int)0)); } set { SetData("UserNo", value); } }
 		public int CountryNo { get { return Convert.ToInt32(GetData("CountryNo", (int)0)); } }
 		public int ResumeAcademicAbilityNo { get { return Convert.ToInt32(GetData("ResumeAcademicAbilityNo", (int)0)); } }
 		public string SchoolName { get { return Convert.ToString(GetData("SchoolName", string.Empty)); } }
