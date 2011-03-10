8e2bf23d9cf682eb3574d4af87642e9a3b520391
ljymill <ljymill@d8e7726e-33f6-2583-9b26-3a411985976f>
2011-03-10 06:45:49 +0000

    면접요청 완료

---
+++ Library/DB/Participate/DataAccess/ParticipateCreate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+
+using Com.Framework.Data;
+using Com.Library.DB;
+
+namespace Com.Library.DB.Participate
+{
+    public class ParticipateCreate : DBDataReader<DataEntity, DataEntity>
+    {
+        public ParticipateCreate()
+            : base(new ConnectionString(), "TSP_ParticipateCreate")
+        {
+        }
+
+        protected override void ParameterInitialize()
+        {
+            AddParameter("CompanyNo", SqlDbType.Int, 4, SqlDataInOut.Input);
+            AddParameter("RecruitNo", SqlDbType.Int, 4, SqlDataInOut.Input);
+            AddParameter("UserNo", SqlDbType.Int, 4, SqlDataInOut.Input);
+            AddParameter("RecruitType", SqlDbType.TinyInt, 1, SqlDataInOut.Input);
+        }
+    }
+}
---
+++ Library/DB/Participate/DataAccess/ParticipateGetList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+
+using Com.Framework.Data;
+using Com.Library.DB;
+
+namespace Com.Library.DB.Participate
+{
+    public class ParticipateGetList : DBDataReader<ParticipateEntity, DataEntity>
+    {
+        public ParticipateGetList()
+            : base(new ConnectionString(), "TSP_ParticipateGetList")
+        {
+        }
+
+        protected override void ParameterInitialize()
+        {
+            AddParameter("CompanyNo", SqlDbType.Int, 4, SqlDataInOut.Input);
+            AddParameter("RecruitNo", SqlDbType.Int, 4, SqlDataInOut.Input);
+            AddParameter("UserNo", SqlDbType.Int, 4, SqlDataInOut.Input);
+        }
+    }
+}
