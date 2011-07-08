3dd88069604a85e37f319e5393b58ffb9cfcddba
ljymill <ljymill@d8e7726e-33f6-2583-9b26-3a411985976f>
2011-07-08 12:34:41 +0000

    en site 추가

---
+++ www/EN/Home/Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/EN/WebMaster/ENMaster.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="EN_Home_Default" Title="Untitled Page" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="Header" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Content" Runat="Server">
+<div>
+<%=strContents %>
+</div>
+</asp:Content>
+
---
+++ www/EN/Home/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+using Com.Framework.Data;
+using Com.Library.Page;
+using Com.Library.DB.Board;
+
+using Site.Web.Page;
+
+public partial class EN_Home_Default : SitePage
+{
+    public string strContents = string.Empty;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected override void OnPreRender(EventArgs e)
+    {
+        int BoardNo = 6;
+        int ArticleNo = 0;
+        ArticleGetListArguments arg = new ArticleGetListArguments();
+        arg.PageNo = 1;
+        arg.PageSize = 1;
+        arg.BoardNo = BoardNo;
+        ArticleGetList list = new ArticleGetList();
+        list.SetArguments(arg);
+        List<ArticleEntity> record = list.Execute();
+        OutputEntity outputdata = list.GetOutput();
+
+        if (record.Count > 0)
+        {
+            ArticleNo = record[0].ArticleNo;
+            ArticleGetInfoArguments argi = new ArticleGetInfoArguments();
+            argi.BoardNo = BoardNo;
+            argi.ArticleNo = ArticleNo;
+            argi._AddReadCount = 0;
+            ArticleGetInfo info = new ArticleGetInfo();
+            info.SetArguments(argi);
+            info.Execute();
+            ArticleEntity m_AricleInfo = info.GetOutput();
+            strContents = m_AricleInfo.ArticleContent;
+        }
+    }
+}
